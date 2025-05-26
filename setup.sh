a8c59b463b9083a969f55b8389e141c5b7fd5110
Jerome Lim <jerlim@student.42singapore.sg>
2025-05-26 14:52:58 +0800

    Update the setup script to manually install pip to get around sch env not having ensurepip installed

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # Check if virtual environment directory exists
 if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
-    python3 -m venv .venv
+    python3 -m venv --without-pip .venv
 else
     echo "Virtual environment already exists."
 fi
@@ -12,6 +12,15 @@ fi
 # Activate the virtual environment
 echo "Activating virtual environment..."
 source .venv/bin/activate
+
+# Install pip if not already installed
+if [ ! -f ".venv/bin/pip" ]; then
+    echo "Installing pip in the virtual environment..."
+    curl https://bootstrap.pypa.io/get-pip.py | python
+else
+    echo "pip is already installed in the virtual environment."
+fi
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
 # Check if requirements.txt exists, and install dependencies
 if [ -f "requirements.txt" ]; then
